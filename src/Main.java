b1297df9eddef9d68fe59593cec269956f65ac53
23eerick <erickwelberth1@hotmail.com>
2023-10-11 20:48:55 -0300

    reading mentioned data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
+        LinkedList<String> products = new LinkedList<>();
 
+        Scanner product = new Scanner(System.in);
+        System.out.println("Add product");
+
+        String name = product.nextLine();
+
+        System.out.println("the product is " + name);
     }
 }
